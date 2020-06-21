683f30d05972abb7d1e85b8a9e90214dc4f07e18
Sorong <patrick_starzynski@web.de>
2020-06-21 13:42:34 +0200

    added reset button to CapsuleFollowsCube.scene

---
+++ Assets/Scripts/RestartEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Restart))]
+public class RestartEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        Restart restart = target as Restart;
+        if(GUILayout.Button("Restart") && restart != null)
+        {
+            restart.RestartScene();
+        }
+    }
+}
